38c4d512108b7e8f1f8d5bc97d68ab0308bf20a6
Kevin Zhu <kevin.z1989@gmail.com>
2021-02-11 17:16:34 -0500

    Initial commit. Added header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Projects from './Projects';
+import Contact from './Contact';
+import Error from './Error.js'
+
+
+function App(props) {
+  console.log('app props', props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <Switch>
+        <Route exact path='/'><Home/></Route>
+        <Route path='/projects'><Projects/></Route>
+        <Route path='/contact'><Contact/></Route>
+        <Route path='*'><Error/></Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Button from '@material-ui/core/Button';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { withRouter } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    headerOptions: {
+      display: 'flex',
+      flex: 1,
+      justifyContent: 'space-evenly'
+    }
+  },
+}));
+
+const Header = ({history}) => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItem = (pageURL) => {
+    history.push(pageURL);
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          
+          <Typography variant="h6" className={classes.title}>
+            KZ
+          </Typography>
+          {isMobile ? (
+            <div>
+              <IconButton 
+                edge="start" 
+                className={classes.menuButton} 
+                color="inherit" 
+                aria-label="menu"
+                onClick={handleMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={() => setAnchorEl(null)}
+              >
+                <MenuItem onClick={() => handleMenuItem('/')}>Home</MenuItem>
+                <MenuItem onClick={() => handleMenuItem('/projects')}>Projects</MenuItem>
+                <MenuItem onClick={() => handleMenuItem('/contact')}>Contact</MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <div>
+              <Button color='inherit' onClick={() => handleMenuItem('/')}>Home</Button>
+              <Button color='inherit' onClick={() => handleMenuItem('/projects')}>Projects</Button>
+              <Button color='inherit' onClick={() => handleMenuItem('/contact')}>Contact</Button>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default withRouter(Header);
